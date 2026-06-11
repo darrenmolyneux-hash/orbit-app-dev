f3b224247c85dc4ccffbca273dd288e0462d1728
darren molyneux <darren.molyneux@mbhglobal.co.uk>
2026-06-11 19:49:54 +0000

    FIXED DATA BEARING

--- pages/Asset_Bookin_Page/jsobjects/JSObject2/JSObject2.js
+++ pages/Asset_Bookin_Page/jsobjects/JSObject2/JSObject2.js
@@ -1,11 +1,27 @@
 export default {
   async getItemTypes() {
     await Get_ItemTypes.run();
-    return Get_ItemTypes.data.map(d => ({ label: d.item_type_name, value: d.item_type_id + "" }));
+
+    return Get_ItemTypes.data.map(d => ({
+      label: d.item_type_name,
+      value: d.item_type_id + ""
+    }));
   },
+
   async selectItemType(value) {
-    await storeValue('selectedItemType', value);
+    await storeValue("selectedItemType", value);
+
+    const selectedType = Get_ItemTypes.data.find(
+      d => String(d.item_type_id) === String(value)
+    );
+
+    await storeValue(
+      "isDataBearing",
+      selectedType?.is_data_bearing === true || selectedType?.is_data_bearing === "true"
+    );
+
     await Get_Makes.run();
+
     return Get_Makes.data;
   }
 }
