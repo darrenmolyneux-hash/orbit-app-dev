6dfb01c35d1a85252d5d189d2af35d04d4d306ba
darren molyneux <darren.molyneux@mbhglobal.co.uk>
2026-06-15 14:29:41 +0000

    monday after many fixes

--- pages/Collection_job_View/jsobjects/JSObject3/JSObject3.js
+++ pages/Collection_job_View/jsobjects/JSObject3/JSObject3.js
@@ -1,17 +1,2 @@
-{{
-  (function() {
-    storeValue('newStatus', [wf_status_changer_widget].model.next_status).then(() => {
-      qry_collection_status_update.run(
-        function() {
-          qry_collection_get.run(() => {
-            qry_status_transitions.run();
-          });
-          showAlert('Status updated to ' + [wf_status_changer_widget].model.next_status, 'success');
-        },
-        function(error) {
-          showAlert('Failed to update status — ' + error.message, 'error');
-        }
-      );
-    });
-  })()
-}}
+export default {
+}
