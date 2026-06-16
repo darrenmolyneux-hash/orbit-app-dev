81ad6e1eef1a3f6f019a54a0c30d76b60d0b90a8
darren molyneux <darren.molyneux@mbhglobal.co.uk>
2026-06-16 14:25:04 +0000

    16 june

--- pages/Asset_Page/jsobjects/StatusActions/StatusActions.js
+++ pages/Asset_Page/jsobjects/StatusActions/StatusActions.js
@@ -1,13 +1,12 @@
 export default {
   async init() {
     await qry_GetAssetById.run();
-    AssetState.oldStatus = qry_GetAssetById.data[0].status;
+    AssetState.oldStatus = qry_GetAssetById.data[0].status_id;
   },
-
   async changeStatus() {
     await qry_UpdateAssetStatus.run();
     await InsertAssetAuditLog.run();
-    AssetState.oldStatus = qry_GetAssetById.data[0].status;
+    AssetState.oldStatus = qry_GetAssetById.data[0].status_id;
     await GetAssetAuditLog.run();
   }
 }
