de42f7c37e1b6f0c1fc9cd314242c256f2d3374e
darren molyneux <darren.molyneux@mbhglobal.co.uk>
2026-06-17 07:31:11 +0000

    test

--- pages/Asset_Page/jsobjects/JSObject1/JSObject1.js
+++ pages/Asset_Page/jsobjects/JSObject1/JSObject1.js
@@ -1,11 +1,12 @@
 export default {
   updateStatus: async () => {
+    showAlert('updateStatus fired', 'success');
     const newStatusId = Custom5.model.selectedStatusId;
-    console.log('updating to:', newStatusId);
     await UpdateAssetStatus.run();
+    await InsertAssetAuditLog.run();
+    await qry_insert_status_history.run();
     await qry_GetAssetById.run();
   },
-
   viewSale: () => {
     const saleId = Custom5.model.saleId;
     if (!saleId) {
@@ -14,18 +15,15 @@ export default {
     }
     navigateTo('SaleDetails', { sale_id: saleId }, 'SAME_WINDOW');
   },
-
   saveCost: async () => {
     await UpdateOriginalCost.run();
     await qry_GetAssetById.run();
   },
-
   onTabChange: async () => {
     if (Tabs1.selectedTab === 'Grading') {
       await Query3qry_grading_get.run();
     }
   },
-
   saveGrading: async () => {
     await qry_grading_save.run();
     await qry_update_overall_grade.run();
---
+++ pages/Home/jsobjects/JSObject2/JSObject2.js
@@ -0,0 +1,5 @@
+export default {
+	Custom1goHome () {
+		//	write code here
+	}
+}
