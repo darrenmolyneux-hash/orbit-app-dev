7cf0b69d2a8036e1c24e6537ec5f7d6422fc287a
darren molyneux <darren.molyneux@mbhglobal.co.uk>
2026-06-18 12:10:33 +0000

    -

--- pages/Asset_Page/jsobjects/AssetActions/AssetActions.js
+++ pages/Asset_Page/jsobjects/AssetActions/AssetActions.js
@@ -1,43 +1,35 @@
 export default {
-
   updateStatus: async () => {
     const newStatusId = Custom5.model.selectedStatusId;
     storeValue('pending_status_id', newStatusId);
-
     if (newStatusId === 15) {
       showModal('Modal1');
       return;
     }
-
     await UpdateAssetStatus.run();
     await InsertAssetAuditLog.run();
     await qry_insert_status_history.run();
     await qry_GetAssetById.run();
     storeValue('pending_status_id', null);
   },
-
   cancelHarvest: () => {
     storeValue('pending_status_id', null);
     qry_GetAssetById.run();
   },
-
   confirmHarvest: async () => {
     try {
       await UpdateAssetStatus.run();
       await InsertAssetAuditLog.run();
       await qry_insert_status_history.run();
-      await qry_GetAssetById.run();
-      JSParts.init();
       closeModal('Modal1');
       storeValue('pending_status_id', null);
-      Tabs1.selectedTab = 'Add/Remove parts';
-      showAlert('Asset sent for parts harvesting. Log components below.', 'success');
+      showAlert('Asset status updated. Starting pre-inventory...', 'success');
+      navigateTo('PreInventory_Page', { asset_id: appsmith.URL.queryParams.asset_id }, 'SAME_WINDOW');
     } catch (err) {
       showAlert('Error updating status: ' + err.message, 'error');
       console.log('confirmHarvest error:', err);
     }
   },
-
   viewSale: () => {
     const saleId = Custom5.model.saleId;
     if (!saleId) {
@@ -46,12 +38,10 @@ export default {
     }
     navigateTo('SaleDetails', { sale_id: saleId }, 'SAME_WINDOW');
   },
-
   saveCost: async () => {
     await UpdateOriginalCost.run();
     await qry_GetAssetById.run();
   },
-
   onTabChange: async () => {
     if (Tabs1.selectedTab === 'Grading') {
       await Query3qry_grading_get.run();
@@ -60,11 +50,9 @@ export default {
       await JSParts.init();
     }
   },
-
   saveGrading: async () => {
     await qry_grading_save.run();
     await qry_update_overall_grade.run();
     await qry_GetAssetById.run();
   }
-
 }
